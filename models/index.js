edac82f923b623a7979816870cec26f9d053f55c
Enrique Jimenez <ejimenez2292@gmail.com>
2022-05-06 00:19:06 -0500

    updated index.js so tags now test properly

--- models/index.js
+++ models/index.js
@@ -20,7 +20,7 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
 })
